15d4fdb5f220d67863af3b8d44f21501481b6cc5
VikramBaraiya <contactbaraiya@gmail.com>
2019-02-15 23:03:24 -0500

    with functionality that allows m to view pending and created recycler view for pending and inserted temp data

--- app/src/main/java/yadunandan/shubhamservice/MainActivity.java
+++ app/src/main/java/yadunandan/shubhamservice/MainActivity.java
@@ -1,11 +1,11 @@
 package yadunandan.shubhamservice;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -104,4 +105,5 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
--- app/src/main/java/yadunandan/shubhamservice/pending.java
+++ app/src/main/java/yadunandan/shubhamservice/pending.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class pending extends Fragment {
+    RecyclerView rv;
+    PendingRecyclerJavaClass prjv;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -18,6 +22,17 @@ public class pending extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        String[] Name={"vikram","ravi","krushit","haard","maulik","meet","kd"};
+        String[] Type={"Repair","Repair","Repair","Repair","Repair","new ","cancel"};
+        String[] Number={"9601019977","9601019988","9601019999","9601019900","9601019911","324324234","34324234"};
+        String[] Adress={"abc","def","ghi","jkl","mno","fdsfsdfs","fdsfsdfsffsd"};
+        String[] Summary={"pqr","stu","vwx","yz","thats it","dsfsdfsdf","dfsfsfdsfds"};
+
+        rv=view.findViewById(R.id.rv);
+        //view.Context c=getActivity();
+        prjv=new PendingRecyclerJavaClass(getActivity(),Name,Type,Number,Adress,Summary);
+        rv.setAdapter(prjv);
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 
 }
