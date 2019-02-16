32caa115584190e4e1479548b202e0ce35c46ec2
VikramBaraiya <contactbaraiya@gmail.com>
2019-02-15 23:30:24 -0500

    implemented the functionlaity of button to change the status of service request

--- app/src/main/java/yadunandan/shubhamservice/pending.java
+++ app/src/main/java/yadunandan/shubhamservice/pending.java
@@ -9,10 +9,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class pending extends Fragment {
     RecyclerView rv;
     PendingRecyclerJavaClass prjv;
+    ImageButton imgbtn1;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,12 +29,22 @@ public class pending extends Fragment {
         String[] Number={"9601019977","9601019988","9601019999","9601019900","9601019911","324324234","34324234"};
         String[] Adress={"abc","def","ghi","jkl","mno","fdsfsdfs","fdsfsdfsffsd"};
         String[] Summary={"pqr","stu","vwx","yz","thats it","dsfsdfsdf","dfsfsfdsfds"};
-
+        String[] StatusOfButton={"pending","pending","pending","pending","pending","pending","pending"};
         rv=view.findViewById(R.id.rv);
         //view.Context c=getActivity();
-        prjv=new PendingRecyclerJavaClass(getActivity(),Name,Type,Number,Adress,Summary);
+        prjv=new PendingRecyclerJavaClass(getActivity(),Name,Type,Number,Adress,Summary,StatusOfButton);
         rv.setAdapter(prjv);
         rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        //imgbtn1=(ImageButton)view.findViewById(R.id.issueResolved);
+        //imgbtn1.setOnClickListener(new view.hasOnClickListeners()
+        //{
+          //  public void onclick ()
+            //{
+
+            //}
+        //});
     }
+
+
 
 }
